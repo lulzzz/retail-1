d790d7c85d0383d0c51b6a6b489ac8734e87e9ca
LiUhoNgDan <814074992@qq.com>
2017-06-15 18:07:22 +0800

    修改冲突

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home.routing.module';
-<<<<<<< a570c0cf0b23b075ce134630bb69efc07d3361fd
-import { HomeComponent } from './home.component';
-import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
-import { HeaderComponent } from '../shared/components/header/header.component';
-=======
 import { SharedUIModule } from '../shared/modules/shared.ui.module';
->>>>>>> 修改头部和侧边栏
+
 
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from '../shared/components/header/header.component';
