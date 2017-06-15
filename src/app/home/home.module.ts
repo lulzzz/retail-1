a570c0cf0b23b075ce134630bb69efc07d3361fd
DESKTOP-QI9PL1E\lee <562050851@qq.com>
2017-06-15 16:47:16 +0800

    更新首页

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home.routing.module';
 import { HomeComponent } from './home.component';
+import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 import { SharedUiModule } from '../shared/shared-ui.module';
 import { IndexComponent } from './index/index.component';
@@ -16,7 +18,9 @@ import { IndexComponent } from './index/index.component';
   ],
   declarations: [
     HomeComponent,
-    IndexComponent
+    IndexComponent,
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 
